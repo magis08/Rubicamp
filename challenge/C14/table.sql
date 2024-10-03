891200fb6a968ab4af19929ed8542fed54cd729f
magis08 <agis7766@gmail.com>
2024-10-03 19:28:52 +0700

    C14

--- challenge/C14/table.sql
+++ challenge/C14/table.sql
@@ -28,6 +28,18 @@ CREATE TABLE matakuliah (
     FOREIGN KEY (dosen) REFERENCES dosen(nip)
 );
 
+-- Buat tabel assign
+CREATE TABLE assignment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nim CHAR(8) NOT NULL,
+    kodemk CHAR(5) NOT NULL,
+    nip CHAR(8) NOT NULL,
+    nilai VARCHAR(3),
+    FOREIGN KEY(nim) REFERENCES mahasiswa(nim),
+    FOREIGN KEY(kodemk) REFERENCES matakuliah(kodemk),
+    FOREIGN KEY(nip) REFERENCES dosen(nip)
+);
+
 -- Menambahkan data jurusan
 INSERT INTO jurusan (kodejurusan, namajurusan) VALUES ('IF', 'Informatika');
 INSERT INTO jurusan (kodejurusan, namajurusan) VALUES ('SI', 'Sistem Informasi');
@@ -43,3 +55,7 @@ INSERT INTO dosen (nip, namadosen) VALUES ('67890', 'Ancelotti');
 -- Menambahkan data matakuliah
 INSERT INTO matakuliah (kodemk, namamk, sks, dosen) VALUES ('MK001', 'Pemrograman Dasar', 3, '12345');
 INSERT INTO matakuliah (kodemk, namamk, sks, dosen) VALUES ('MK002', 'Basis Data', 3, '67890');
+
+-- Menambahkan nilai Mahasiswa
+INSERT INTO assignment (nim, kodemk, nip, nilai) VALUES ('A001', 'MK001', '12345', 'A');
+INSERT INTO assignment (nim, kodemk, nip, nilai) VALUES ('A002', 'MK001', '12345', 'A');
