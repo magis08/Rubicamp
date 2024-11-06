43e9ddc531fe736e7e7d3923377f919fb23ea4f7
magis08 <agis7766@gmail.com>
2024-11-06 14:27:21 +0700

    edit c21

--- challenge/C21/routes/index.js
+++ challenge/C21/routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 module.exports = function (db) {
 
-
-  /* GET home page. */
   router.get('/', function (req, res, next) {
     db.query('SELECT * FROM todos', (err, data) => {
       if (err) return res.send(err)
       res.render('list', { data: data.rows });
     })
-
   });
+
+  router.get('/add', function (req, res, next) {
+res.render('form')
+  })
 
   return router;
 }
